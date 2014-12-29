d66c5b2a72cff11808f46de660ee2889a1178a29
alokhm <akshukla226@gmail.com>
2014-12-29 12:48:43 +0530

    Update detect.sh

--- bin/detect.sh
+++ bin/detect.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # bin/use <build-dir>
-echo "This will go into the body of the mail." | mail -s "Hello world" akshukla226@gmail.com
+#echo "This will go into the body of the mail." | mail -s "Hello world" akshukla226@gmail.com
 #exec 1> >(logger -s -t $(basename $0)) 2>&1
 if [ -f $1/index.html ]; then
    echo "Java" && exit 0
